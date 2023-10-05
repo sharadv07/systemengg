7ebcf8f18096712eb01b56a61f56a7b930e47659
shardv07 <sharad.vidyapathy@gmail.com>
2023-10-05 16:11:14 -0400

    Debgging

--- storage_account.tf
+++ storage_account.tf
@@ -2,7 +2,7 @@
 resource "azurerm_storage_account" "storage_account" {
     for_each = local.storage_account
         name = each.value.name
-        resource_group_name = data.tfe_outputs.resource_group.nonsensitive_values.name
+        resource_group_name = data.tfe_outputs.resource_group.name
         location = data.tfe_outputs.resource_group.location
         account_tier = "Standard"
         account_replication_type = "LRS"
