dc377efc39b17c8d62d392e31f69b100a77333df
shardv07 <sharad.vidyapathy@gmail.com>
2023-10-05 03:41:07 -0400

    fixed outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "resource_group_name_output" {
-  value = data.tfe_outputs.resource_group.values.name
+  value = [data.tfe_outputs.resource_group.values.name]
 }
 
 output "resource_group_loc_out" {
-  value = data.tfe_outputs.resource_group.values.location
+  value = [data.tfe_outputs.resource_group.values.location]
 }
