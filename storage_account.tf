df217413e202581e8a0ca8668c1b559dd4413319
shardv07 <sharad.vidyapathy@gmail.com>
2023-10-05 15:42:55 -0400

    debugging

--- storage_account.tf
+++ storage_account.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_account" "storage_account" {
     for_each = local.storage_account
         name = each.value.name
         resource_group_name = data.tfe_outputs.resource_group.nonsensitive_values.name
-        location = data.tfe_outputs.resource_group.nonsensitive_values.location
+        location = data.tfe_outputs.resource_group.location
         account_tier = "Standard"
         account_replication_type = "LRS"
 
