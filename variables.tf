f5391c834ca71ec9a7bbcbf1715ed2c2856e75bc
shardv07 <sharad.vidyapathy@gmail.com>
2023-10-06 17:10:16 -0400

    debug issue - 1

--- variables.tf
+++ variables.tf
@@ -4,6 +4,17 @@ variable "storage_account_list" {
   description = "list of storage account objects "
 }
 
+variable "resource_group_name" {
+  type        = string
+  description = "resource group name "
+}
+
+variable "location" {
+  type        = string
+  description = "location "
+}
+
+
 variable "default_values" {
   type        = any
   default     = {}
